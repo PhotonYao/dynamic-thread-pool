d2967fda6945312b575be070a048c58ef06fad88
K·Herbert <herbert501@qq.com>
2024-09-13 20:32:17 +0800

    初始化工程

---
+++ dynamic-thread-pool-springboot-starter/src/main/java/top/kangyaocoding/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,10 @@
+package top.kangyaocoding.middleware.dynamic.thread.pool.sdk.config;
+
+/**
+ * 描述:
+ *
+ * @author K·Herbert
+ * @since 2024-09-13 20:28
+ */
+public class DynamicThreadPoolAutoConfig {
+}
---
+++ dynamic-thread-pool-springboot-starter/src/main/java/top/kangyaocoding/middleware/dynamic/thread/pool/sdk/tigger/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * 描述:
+ *
+ * @author K·Herbert
+ * @since 2024-09-13 20:31
+ */
+package top.kangyaocoding.middleware.dynamic.thread.pool.sdk.tigger;
